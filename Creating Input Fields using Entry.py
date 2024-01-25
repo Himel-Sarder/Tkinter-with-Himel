6928e5b6979ed3ab9222cf5a00929cb616177400
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-01-25 23:30:41 +0600

    Tkinder #7

---
+++ Creating Input Fields using Entry.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+myEntry = Entry(root, text="Enter a name", bg="Orange", fg="Black")
+myEntry.pack()
+
+root.mainLoop()
+
+#Himel Sarder
+#Dept. of CSE, BSFMSTU
