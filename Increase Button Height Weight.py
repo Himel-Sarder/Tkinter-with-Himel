dd0597bfd5ae5c79dfea485cff321cc418689640
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-01-25 22:45:54 +0600

    Tkinder #4

---
+++ Increase Button Height Weight.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+myB = Button(root, text="Click Me", padx=40, pady=20)  #padx increase weight
+                                                       #pady increase height
+myB.pack()
+
+root.mainloop()
+
+#Himel Sarder
+#Dept. of CSE, BSFMSTU
