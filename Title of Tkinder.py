235a5e4e5ec5644cfab5cde6b8b12bd9099f2777
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-01-26 01:21:02 +0600

    Tkinder #11

---
+++ Title of Tkinder.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+root = Tk()
+
+
+root.title("Himel's Tkinter")
+
+
+
+myEntry = Entry(root, bg="Orange", fg="Black", borderwidth=2)
+myEntry.pack()
+myEntry.insert(0, "Enter your name:")
+def myClick():
+    myLabel = Label(root, text="Hello " + myEntry.get() + "! Meet with Himel")
+    myLabel.pack()
+
+myButton = Button(root, text="Click", command=myClick, fg="Green", bg="Pink")
+myButton.pack()
+
+root.mainloop()
+
+#Himel Sarder
+#Dept. of CSE, BSFMSTU
