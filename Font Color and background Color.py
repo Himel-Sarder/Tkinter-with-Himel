17f0039153c458c194732c06ad35ae186325c1d7
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-01-25 23:15:16 +0600

    Tkinder #6

---
+++ Font Color and background Color.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+
+def myClick():
+    myLabel = Label(root, text="Hello!! I am Himel", fg="Blue", bg="Yellow")    #fg = font color    #bg = background color
+    myLabel.pack()
+
+myButton = Button(root, text="Click Me", padx=40, pady=20, command=myClick, fg="Green", bg="Pink")
+myButton.pack()
+
+root.mainloop()
+
+#Himel Sarder
+#Dept. of CSE, BSFMSTU
