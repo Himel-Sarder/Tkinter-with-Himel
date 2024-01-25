75b79b321a378a56f9ce1cc2541ca51ca16ee2e7
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-01-25 23:35:17 +0600

    Tkinder #8

---
+++ Increase Border width in Entry.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+
+myEntry = Entry(root, text="Enter a name", bg="Orange", fg="Black", bd=5) 
+#myEntry = Entry(root, text="Enter a name", bg="Orange", fg="Black", borderwidth=5) 
+
+myEntry.pack()
+
+#Himel Sarder
+#Dept. of CSE, BSFMSTU
