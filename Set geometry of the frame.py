0e9c776d5e4fbfc47fc1f0f8907d2b45872efad3
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-01-27 03:34:00 +0600

    Tkinter #14

---
+++ Set geometry of the frame.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+#Create an instance of tkinter frame
+root= Tk()
+
+#Set the geometry of frame
+root.geometry("600x400")
+
+#Create a text label
+Label(root, text="Geometry of the frame").pack(pady=20)
+
+root.mainloop()
