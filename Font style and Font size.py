1be826b7eb9a59f7da930255343bd8204c1b3ebd
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-01-27 03:37:58 +0600

    Tkinter #15

---
+++ Font style and Font size.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+#Create an instance of tkinter frame
+root= Tk()
+
+#Create a text label
+myLabel = Label(root, text="Himel Sarder, student of CSE, lives in Khulna", font=('Times
+New Roman bold',15))
+myLabel.pack()
+
+root.mainloop()
