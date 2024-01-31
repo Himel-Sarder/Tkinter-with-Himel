b4cf1dee8e078d9a4a24db9af0b8d6abe513a43b
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-01-31 23:56:19 +0600

    Tlinter #23

---
+++ MessageBox.py
@@ -0,0 +1,90 @@
+'''The `tkinter` module in Python provides the `messagebox` module, 
+which allows you to create and display various types of message boxes. 
+Here are some commonly used message boxes:
+'''
+
+
+
+1. **showinfo():** This displays an information message box.
+--------------------------------------------------------------------------  
+    from tkinter import messagebox
+
+    messagebox.showinfo("Information", "This is an information message.")
+--------------------------------------------------------------------------
+
+
+
+
+2. **showwarning():** This displays a warning message box.
+--------------------------------------------------------------------------
+    from tkinter import messagebox
+
+    messagebox.showwarning("Warning", "This is a warning message.")
+--------------------------------------------------------------------------
+
+
+
+3. **showerror():** This displays an error message box.
+--------------------------------------------------------------------------
+    from tkinter import messagebox
+
+    messagebox.showerror("Error", "This is an error message.")
+--------------------------------------------------------------------------
+
+
+
+4. **askquestion():** This displays a yes/no question dialog.
+-------------------------------------------------------------------------
+    from tkinter import messagebox
+
+    response = messagebox.askquestion("Question", "Do you want to proceed?")
+    if response == 'yes':
+        print("User clicked Yes.")
+    else:
+        print("User clicked No.")
+--------------------------------------------------------------------------
+
+
+
+5. **askyesno():** This displays a yes/no dialog.
+--------------------------------------------------------------------------
+    from tkinter import messagebox
+
+    response = messagebox.askyesno("Yes/No", "Do you want to continue?")
+    if response:
+        print("User clicked Yes.")
+    else:
+        print("User clicked No.")
+--------------------------------------------------------------------------
+
+
+
+6. **askokcancel():** This displays an OK/Cancel dialog.
+--------------------------------------------------------------------------
+    from tkinter import messagebox
+
+    response = messagebox.askokcancel("OK/Cancel", "Do you want to cancel?")
+    if response:
+        print("User clicked OK.")
+    else:
+        print("User clicked Cancel.")
+--------------------------------------------------------------------------
+
+
+
+7. **askretrycancel():** This displays a Retry/Cancel dialog.
+--------------------------------------------------------------------------
+    from tkinter import messagebox
+
+    response = messagebox.askretrycancel("Retry/Cancel", "Do you want to retry?")
+    if response:
+        print("User clicked Retry.")
+    else:
+        print("User clicked Cancel.")
+--------------------------------------------------------------------------
+
+
+'''
+These message boxes can be useful for communicating with the user and obtaining their input in different scenarios. 
+You can choose the appropriate type of message box based on the nature of the information you want to convey or 
+the decision you want the user to make.'''
