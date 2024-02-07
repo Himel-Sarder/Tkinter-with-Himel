7f5d68ca62d5ab62b0b0b69ad56ab07b9130d16c
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2024-02-07 16:36:09 +0600

    Tkinter #36

---
+++ Simple Checkbox.py
@@ -0,0 +1,11 @@
+from tkinter import Tk, Checkbutton
+root = Tk()
+root.title("Himel's Checkbox")
+
+
+# Create checkboxes
+checkbox1 = Checkbutton(root, text="Checkbox 1")
+checkbox1.pack()
+
+
+root.mainloop()
